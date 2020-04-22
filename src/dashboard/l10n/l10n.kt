115623d0be44c186efd28f08b5b31bc05f972ce3
Dmitry Tsydzik <tsyd@tut.by>
2020-04-22 23:43:12 +0300

    Fixed language detection

--- src/dashboard/l10n/l10n.kt
+++ src/dashboard/l10n/l10n.kt
@@ -2,7 +2,7 @@ package dashboard.l10n
 
 import kotlin.browser.window
 
-fun getL10n(): L10n = when (window.navigator.language) {
-    "ru-RU" -> Russian
+fun getL10n(): L10n = when (window.navigator.language.toLowerCase()) {
+    "ru-ru", "ru" -> Russian
     else -> English
 }
